7a8a5bd931322ec7de1d82d5683ecd914d42129f
Admin <noobestman@gmail.com>
2022-11-19 11:16:53 +0530

    fileupload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const upload = require('express-fileupload')
+
+
+const app = express()
+
+app.use(upload())
+
+app.get("/",(req,res)=> {
+    res.sendFile(__dirname + '/index.htm')
+})
+
+
+app.post('/',(req,res)=>{
+    if(req.files) {
+        console.log(req.files)
+        var file = req.files.file
+        var filename = file.name
+        console.log(filename,file.mimetype)
+        file.mv('/uploads/',filename,(err)=>{
+            if(err) {
+                console.log(err)
+                res.send(err)
+            }   res.send("file uploaded!")
+        })
+    }
+})
+
+app.listen(5000)
